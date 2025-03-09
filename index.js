bcc3a757d8e082d2b553fa1ede6c1be49dc7effe
golap-raj <golapraj47@gmail.com>
2025-03-10 00:48:01 +0600

    resolve

---
+++ index.js
@@ -0,0 +1,18 @@
+// class person {
+//     malik = "Golap"
+
+//     constructor(name,age, malik){
+//         this.name = name;
+//         this.age = age
+//         this.malik = malik
+
+//     }
+
+//     person3(){
+//         console.log(`my name is ${this.name} I am ${this.age} years Old`)
+//     }
+
+// }
+
+// const person1 = new person ("Safayet", "27", "Raj");
+// console.log(person1.person3())
